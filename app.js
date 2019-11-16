51457c9b7cd1d4a707abce1d49eda2b0eeb4e95c
SinChee <37462165+SinChee@users.noreply.github.com>
2019-11-16 11:37:38 +0800

    init

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Set constraints for the video stream
-var constraints = { video: { facingMode: {exact:'environment'} }, audio: false };
+var constraints = { video: { facingMode: 'environment' }, audio: false };
 
 var config = {
     apiKey: "AIzaSyBSOUBCNLrJULyPy9e-TQsOw0XkfxSfWAA",
