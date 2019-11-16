179ece1d37d71298d9bbe1b8ed3d393f5910b765
SinChee <37462165+SinChee@users.noreply.github.com>
2019-11-16 10:50:19 +0800

    init

---
+++ app.js
@@ -0,0 +1,66 @@
+// Set constraints for the video stream
+var constraints = { video: { facingMode: "user" }, audio: false };
+
+var config = {
+    apiKey: "AIzaSyBSOUBCNLrJULyPy9e-TQsOw0XkfxSfWAA",
+    authDomain: "polar-storm-240813.firebaseapp.com",
+    databaseURL: "https://polar-storm-240813.firebaseio.com",
+    storageBucket: "polar-storm-240813.appspot.com",
+    messagingSenderId: "572073336908",
+};
+firebase.initializeApp(config);
+
+var db = firebase.database().ref('cam/text');
+var MagicPowder = false;
+var MagicGrade = "";
+var MagicRipe = "";
+var MagicPerc = "";
+
+fireBase_Data.on("value", function (snapshot) {
+    MagicPowder = snapshot.child('override_model').val();
+    MagicGrade = snapshot.child('grade').val();
+    MagicRipe = snapshot.child('ripe').val();
+    MagicPerc = snapshot.child('percentage').val();
+}, function (errorObject) {
+    console.log("The read failed: " + errorObject.code);
+});
+
+// Define constants
+const cameraView = document.querySelector("#camera--view"),
+    cameraOutput = document.querySelector("#camera--output"),
+    cameraSensor = document.querySelector("#camera--sensor"),
+    cameraTrigger = document.querySelector("#camera--trigger")
+    cameraText = document.querySelector("#camera--text")
+// Access the device camera and stream to cameraView
+function cameraStart() {
+    navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then(function(stream) {
+        track = stream.getTracks()[0];
+        cameraView.srcObject = stream;
+    })
+    .catch(function(error) {
+        console.error("Oops. Something is broken.", error);
+    });
+}
+// Take a picture when cameraTrigger is tapped
+cameraTrigger.onclick = function() {
+    cameraSensor.width = cameraView.videoWidth;
+    cameraSensor.height = cameraView.videoHeight;
+    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+    cameraOutput.src = cameraSensor.toDataURL("image/webp");
+    cameraOutput.classList.add("taken");
+    cameraText.innerHTML="Uploading File"
+    setTimeout(donothing,1000);
+};
+
+function donothing(){
+    console.log("Hey")
+    db.on('value', function(snapshot) {
+        console.log(snapshot.val());
+        cameraText.innerHTML=snapshot.val()
+    });
+    
+}
+// Start the video stream when the window loads
+window.addEventListener("load", cameraStart, false);
